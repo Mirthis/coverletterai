fbbe8790d328f3c492fc27d307e8d6c2937a85e4
mirthis@gmail.com <mirthis@gmail.com>
2024-03-18 11:25:54 +0000

    update README.md

--- src/server/trpc/router/coverLetters.ts
+++ src/server/trpc/router/coverLetters.ts
@@ -60,6 +60,7 @@ export const coverLettersRouter = router({
       }
 
       const userId = ctx.session?.user ? ctx.session.user.id : null;
+
       const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: generatePrompt(input),
